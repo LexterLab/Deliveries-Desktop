a51778e503cb9ab424122ff814684c691d040d70
moldovexc <alexanderparpulansky@gmail.com>
2024-03-17 22:44:31 +0200

    Adding payload for sign in and sign up

---
+++ src/main/java/com/tuvarna/delivery/authentication/payload/request/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package com.tuvarna.delivery.authentication.payload.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+
+public record LoginRequestDTO(
+        @Schema(example = "user")
+        @NotEmpty(message = "Please enter your username")
+        String email,
+
+        @NotEmpty(message = "Please enter your password")
+        @Schema(example = "!user123")
+        String password
+) { }
---
+++ src/main/java/com/tuvarna/delivery/authentication/payload/request/SignUpRequestDTO.java
@@ -0,0 +1,49 @@
+package com.tuvarna.delivery.authentication.payload.request;
+
+import com.tuvarna.delivery.authentication.payload.validator.PasswordValueMatches;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
+@PasswordValueMatches.List({
+        @PasswordValueMatches(
+                field = "password",
+                fieldMatch = "matchingPassword"
+        )
+})
+public record SignUpRequestDTO (
+        @Schema(example = "James")
+        @NotEmpty(message = "Please enter a first name.")
+        @Length(min = 1, max = 50, message = "First name must not exceed 50 characters.")
+        @Pattern(regexp = "^[A-Za-z'-. ]{1,50}$", message = "Please enter a valid first name.")
+        String firstName,
+
+        @Schema(example = "Bond")
+        @NotEmpty(message = "Please enter a last name.")
+        @Length(min = 1, max = 50, message = "Last name must not exceed 50 characters.")
+        @Pattern(regexp = "^[A-Za-z'-. ]{1,50}$", message = "Please enter a valid last name.")
+        String lastName,
+
+        @Schema(example = "!User123")
+        @NotEmpty(message = "Please enter a password.")
+        @Size(min = 8, message = "Password must be at least 8 characters long.")
+        @Size(max = 100, message = "Password must not exceed 100 characters.")
+        String password,
+
+        @Schema(example = "!User123")
+        @NotEmpty(message = "Please enter a matching password.")
+        @Size(min = 8, message = "Matching Password should be at least 8 characters long.")
+        @Size(max = 100, message = "Matching Password must not exceed 100 characters.")
+        String matchingPassword,
+        @Schema(example = "+359888123456")
+        @NotEmpty(message = "Please enter a phone number.")
+        @Pattern(regexp ="^\\+?[1-9][0-9]{7,14}$", message = "Please enter a valid phone number.")
+        @Length(min = 7, max = 14, message = "Phone number must be 7-14 characters long.")
+        String phoneNumber,
+        @Schema(example = "123 Main Street LA, CA 12345")
+        @NotEmpty(message = "Please enter an address.")
+        @Length(min = 8, max = 150, message = "Address must be 8-150 characters long")
+        String address
+){}
