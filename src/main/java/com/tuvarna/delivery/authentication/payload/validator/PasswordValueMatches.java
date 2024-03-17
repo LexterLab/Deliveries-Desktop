27bda4ba8698b6876e60dccb6718241bd6cae1a3
moldovexc <alexanderparpulansky@gmail.com>
2024-03-17 22:43:55 +0200

    Added custom password validator

---
+++ src/main/java/com/tuvarna/delivery/authentication/payload/validator/PasswordValueMatches.java
@@ -0,0 +1,35 @@
+package com.tuvarna.delivery.authentication.payload.validator;
+
+import com.tuvarna.delivery.utils.Messages;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = PasswordMatchValidator.class )
+public @interface PasswordValueMatches {
+    String message() default Messages.PASSWORDS_DONT_MATCH;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String field();
+
+    String fieldMatch();
+
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        PasswordValueMatches[] value();
+    }
+}
