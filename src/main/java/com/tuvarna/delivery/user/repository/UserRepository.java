b9a48d5a96dcf2540787f1fb27026f5ebf368eee
moldovexc <alexanderparpulansky@gmail.com>
2024-03-17 22:44:19 +0200

    Adding jpa repo method for finding user by username

--- src/main/java/com/tuvarna/delivery/user/repository/UserRepository.java
+++ src/main/java/com/tuvarna/delivery/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.tuvarna.delivery.user.repository;
 import com.tuvarna.delivery.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsernameIgnoreCase(String username);
 }
