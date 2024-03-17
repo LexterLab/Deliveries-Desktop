70491305905cc0ba28c06d4a7816f9453a4c4159
moldovexc <alexanderparpulansky@gmail.com>
2024-03-17 22:41:26 +0200

    Adding swagger doc

--- src/main/java/com/tuvarna/delivery/DeliveryApplication.java
+++ src/main/java/com/tuvarna/delivery/DeliveryApplication.java
@@ -1,9 +1,20 @@
 package com.tuvarna.delivery;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Delivery API",
+        description = "Delivery Endpoints Documentation",
+        version = "v1.0",
+        contact = @Contact(
+                name = "Alexander Parpulansky",
+                email = "alexanderparpulansky@gmail.com"
+        )))
 public class DeliveryApplication {
 
     public static void main(String[] args) {
