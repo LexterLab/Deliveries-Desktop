fa5a732515b4087b0998620c6973e68aca825d05
moldovexc <alexanderparpulansky@gmail.com>
2024-03-23 19:28:16 +0200

    added creation of couriers

--- src/main/java/com/tuvarna/delivery/office/controller/OfficeController.java
+++ src/main/java/com/tuvarna/delivery/office/controller/OfficeController.java
@@ -2,20 +2,40 @@ package com.tuvarna.delivery.office.controller;
 
 import com.tuvarna.delivery.office.payload.request.CourierRequestDTO;
 import com.tuvarna.delivery.office.service.OfficeService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/offices")
+@Tag(name = "Office REST APIs")
 public class OfficeController {
     private final OfficeService officeService;
 
-
+    @Operation(
+            summary = "Enlist Courier REST API",
+            description = "Enlist Courier REST API is used to add a courier to office"
+    )
+    @ApiResponses( value = {
+            @ApiResponse( responseCode = "201", description = "Http Status 201 CREATED"),
+            @ApiResponse( responseCode = "401", description = "Http Status 401 UNAUTHORIZED"),
+            @ApiResponse( responseCode = "403", description = "Http Status 403 FORBIDDEN"),
+            @ApiResponse( responseCode = "404", description = "Http Status 404 NOT FOUND")
+    })
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("{officeId}/couriers")
     public ResponseEntity<CourierRequestDTO> addCourierToOffice(@Schema(example = "1") @PathVariable Long officeId,
                                                                 @RequestBody @Valid CourierRequestDTO requestDTO) {
--- src/main/java/com/tuvarna/delivery/office/service/OfficeService.java
+++ src/main/java/com/tuvarna/delivery/office/service/OfficeService.java
@@ -7,7 +7,9 @@ import com.tuvarna.delivery.office.payload.mapper.CourierMapper;
 import com.tuvarna.delivery.office.payload.request.CourierRequestDTO;
 import com.tuvarna.delivery.office.repository.CourierRepository;
 import com.tuvarna.delivery.office.repository.OfficeRepository;
+import com.tuvarna.delivery.user.model.Role;
 import com.tuvarna.delivery.user.model.User;
+import com.tuvarna.delivery.user.repository.RoleRepository;
 import com.tuvarna.delivery.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,7 @@ public class OfficeService {
     private final CourierRepository courierRepository;
     private final OfficeRepository officeRepository;
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Transactional
     public CourierRequestDTO enlistCourierToOffice(Long officeId, CourierRequestDTO requestDTO) {
@@ -28,6 +31,14 @@ public class OfficeService {
         User user = userRepository.findUserByUsernameIgnoreCase(requestDTO.username())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "username", requestDTO.username()));
 
+        Role courierRole = roleRepository.findByName("ROLE_COURIER")
+                .orElseThrow(() -> new ResourceNotFoundException("Role", "name", "COURIER"));
+
+
+        user.getRoles().add(courierRole);
+
+        userRepository.save(user);
+
         Courier courier = CourierMapper.INSTANCE.dtoToEntity(requestDTO);
         courier.setOffice(office);
         courier.setUser(user);
@@ -35,5 +46,7 @@ public class OfficeService {
 
         return requestDTO;
     }
+
+
 
 }
