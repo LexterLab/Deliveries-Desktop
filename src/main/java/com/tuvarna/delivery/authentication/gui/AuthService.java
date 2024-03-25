aa29f6787ee59ada454567fac4f7d84a85cc55c3
moldovexc <alexanderparpulansky@gmail.com>
2024-03-25 21:38:26 +0200

    Sign up fetch request added

--- src/main/java/com/tuvarna/delivery/authentication/gui/AuthService.java
+++ src/main/java/com/tuvarna/delivery/authentication/gui/AuthService.java
@@ -1,6 +1,7 @@
 package com.tuvarna.delivery.authentication.gui;
 
 import com.tuvarna.delivery.authentication.payload.request.LoginRequestDTO;
+import com.tuvarna.delivery.authentication.payload.request.SignUpRequestDTO;
 import com.tuvarna.delivery.jwt.JWTAuthenticationResponse;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
@@ -16,5 +17,17 @@ public class AuthService {
                 HttpMethod.POST,
                 request,
                 JWTAuthenticationResponse.class);
+    }
+
+    public ResponseEntity<String> fetchSignUp(SignUpRequestDTO requestDTO) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<SignUpRequestDTO> request = new HttpEntity<>(requestDTO, headers);
+        return restTemplate.exchange(
+                "http://localhost:443/api/v1/auth/signup?type=USER",
+                HttpMethod.POST,
+                request,
+                String.class);
     }
 }
