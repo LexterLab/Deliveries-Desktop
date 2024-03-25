efde89991bdb5861e4b97a76e04c3b36c0fdf627
moldovexc <alexanderparpulansky@gmail.com>
2024-03-25 16:10:16 +0200

    added swagger examples for schemas

--- src/main/java/com/tuvarna/delivery/delivery/payload/response/DeliveryDTO.java
+++ src/main/java/com/tuvarna/delivery/delivery/payload/response/DeliveryDTO.java
@@ -1,17 +1,29 @@
 package com.tuvarna.delivery.delivery.payload.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
 public record DeliveryDTO(
+        @Schema(example = "1")
         Long id,
+        @Schema(example = "user")
         String username,
+        @Schema(example = "courier")
         String courierName,
+        @Schema(example = "WAITING FOR COURIER")
         String statusName,
+        @Schema(example = "Varna")
         String fromCityName,
+        @Schema(example = "Sofia")
         String toCityName,
+        @Schema(example = "20.00")
         Double weightKG,
+        @Schema(example = "Skittles")
         String productName,
+        @Schema(example = "20.00")
         Double totalPrice,
+        @Schema(example = "Deliver with care!")
         String details,
         LocalDateTime orderedAt,
         LocalDateTime deliveredAt
--- src/main/java/com/tuvarna/delivery/user/payload/response/UserDTO.java
+++ src/main/java/com/tuvarna/delivery/user/payload/response/UserDTO.java
@@ -1,10 +1,17 @@
 package com.tuvarna.delivery.user.payload.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserDTO(
+        @Schema(example = "1")
         Long id,
-        String fistName,
+        @Schema(example = "User")
+        String firstName,
+        @Schema(example = "User")
         String lastName,
+        @Schema(example = "+359********")
         String phoneNumber,
+        @Schema(example = "User Boulevard")
         String address
 ) {
 }
