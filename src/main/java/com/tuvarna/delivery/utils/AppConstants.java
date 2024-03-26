faeab7c61b0f1bf10ec2851da557c97f365ed8b1
moldovexc <alexanderparpulansky@gmail.com>
2024-03-26 15:58:59 +0200

    Added DOMAIN constant

--- src/main/java/com/tuvarna/delivery/utils/AppConstants.java
+++ src/main/java/com/tuvarna/delivery/utils/AppConstants.java
@@ -5,4 +5,5 @@ public interface AppConstants {
     String DEFAULT_PAGE_SIZE = "10";
     String DEFAULT_SORT_BY = "id";
     String DEFAULT_SORT_DIRECTION = "asc";
+    String DOMAIN = "http://localhost:443/api/v1/";
 }
