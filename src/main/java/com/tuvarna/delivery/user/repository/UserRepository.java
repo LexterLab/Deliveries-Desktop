e306576b26c06aff3e5790b46f4fde3657a74549
moldovexc <alexanderparpulansky@gmail.com>
2024-03-27 20:11:28 +0200

    Reworked retrieving user list to retrieve only normal users

--- src/main/java/com/tuvarna/delivery/user/repository/UserRepository.java
+++ src/main/java/com/tuvarna/delivery/user/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.tuvarna.delivery.user.repository;
 
+import com.tuvarna.delivery.user.model.Role;
 import com.tuvarna.delivery.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsernameIgnoreCase(String username);
     Boolean existsByUsernameIgnoreCase(String username);
+    Page<User> findAllByRolesIsContainingAndRolesIsNotContaining(Role userRole, Role adminRole, Pageable pageable);
 }
+
--- src/main/java/com/tuvarna/delivery/user/service/UserService.java
+++ src/main/java/com/tuvarna/delivery/user/service/UserService.java
@@ -5,11 +5,13 @@ import com.tuvarna.delivery.delivery.payload.response.DeliveryResponse;
 import com.tuvarna.delivery.delivery.repository.DeliveryRepository;
 import com.tuvarna.delivery.delivery.service.helper.DeliveryHelper;
 import com.tuvarna.delivery.exception.ResourceNotFoundException;
+import com.tuvarna.delivery.user.model.Role;
 import com.tuvarna.delivery.user.model.User;
 import com.tuvarna.delivery.user.payload.mapper.UserMapper;
 import com.tuvarna.delivery.user.payload.request.UserRequestDTO;
 import com.tuvarna.delivery.user.payload.response.UserDTO;
 import com.tuvarna.delivery.user.payload.response.UserResponseDTO;
+import com.tuvarna.delivery.user.repository.RoleRepository;
 import com.tuvarna.delivery.user.repository.UserRepository;
 import com.tuvarna.delivery.user.service.helper.UserHelper;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final DeliveryRepository deliveryRepository;
+    private final RoleRepository roleRepository;
     private final DeliveryHelper deliveryHelper;
     private final UserHelper userHelper;
 
@@ -57,9 +60,16 @@ public class UserService {
         Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
                 : Sort.by(sortBy).descending();
 
+        Role userRole = roleRepository.findByName("ROLE_USER")
+                .orElseThrow(() -> new ResourceNotFoundException("Role", "name", "ROLE_USER"));
+
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN")
+                .orElseThrow(() -> new ResourceNotFoundException("Role", "name", "ROLE_ADMIN"));
+
         Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
 
-        Page<User> userPage = userRepository.findAll(pageable);
+        Page<User> userPage = userRepository
+                .findAllByRolesIsContainingAndRolesIsNotContaining(userRole, adminRole, pageable);
 
         return userHelper.getUserResponse(userPage);
     }
