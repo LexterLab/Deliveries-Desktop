8f4c658d823403fa8db81a9ce57a19c00504e336
moldovexc <alexanderparpulansky@gmail.com>
2024-03-27 14:13:38 +0200

    Fetch request for fetching cities

---
+++ src/main/java/com/tuvarna/delivery/gui/service/CityService.java
@@ -0,0 +1,28 @@
+package com.tuvarna.delivery.gui.service;
+
+import com.tuvarna.delivery.city.payload.CityDTO;
+import com.tuvarna.delivery.gui.AccessTokenStorage;
+import com.tuvarna.delivery.utils.AppConstants;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class CityService {
+    public ResponseEntity<List<CityDTO>> fetchAllCities() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(AccessTokenStorage.retrieveAccessToken());
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = AppConstants.DOMAIN + "cities";
+
+        return restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<>() {
+        });
+    }
+}
