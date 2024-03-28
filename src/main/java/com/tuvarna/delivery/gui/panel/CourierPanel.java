fe08a3076b104c81f3a82fad6c25632c41accb94
moldovexc <alexanderparpulansky@gmail.com>
2024-03-28 11:59:58 +0200

    panel displaying all couriers

--- src/main/java/com/tuvarna/delivery/gui/panel/CourierPanel.java
+++ src/main/java/com/tuvarna/delivery/gui/panel/CourierPanel.java
@@ -1,14 +1,17 @@
 package com.tuvarna.delivery.gui.panel;
 
 import com.tuvarna.delivery.gui.model.CourierTableModel;
+import com.tuvarna.delivery.gui.service.CourierService;
 import com.tuvarna.delivery.office.payload.response.CourierDTO;
+import com.tuvarna.delivery.office.payload.response.CourierResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class CourierPanel extends JPanel {
     private final JTable courierTable;
@@ -39,6 +42,30 @@ public class CourierPanel extends JPanel {
     }
 
     private void fetchCouriers() {
+        try {
+            CourierService service = new CourierService();
+            ResponseEntity<CourierResponseDTO> response = service.fetchAllCouriers();
+            setTableData(Objects.requireNonNull(response.getBody()).couriers());
+        } catch (HttpClientErrorException e) {
+            JOptionPane.showMessageDialog(this, "Internal Server Error", "Server Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
+    public void setTableData(List<CourierDTO> couriers) {
+        CourierTableModel tableModel = new CourierTableModel(couriers);
+        courierTable.setModel(tableModel);
+    }
+
+    private void openCourierInfoPanel(CourierDTO courierDTO) {
+        CourierInfoPanel courierInfoPanel = new CourierInfoPanel(courierDTO);
+
+        JDialog dialog = new JDialog();
+        dialog.setTitle("Courier Information");
+        dialog.setModal(true);
+        dialog.setContentPane(courierInfoPanel);
+        dialog.pack();
+        dialog.setLocationRelativeTo(this);
+        dialog.setVisible(true);
     }
 }
