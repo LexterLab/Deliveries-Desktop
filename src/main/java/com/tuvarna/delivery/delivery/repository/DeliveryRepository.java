a40cea92cb0332b2c4a48f5656316fb4d38a0e5c
moldovexc <alexanderparpulansky@gmail.com>
2024-03-30 15:49:52 +0200

    sql request for finding user deliveries by date

--- src/main/java/com/tuvarna/delivery/delivery/repository/DeliveryRepository.java
+++ src/main/java/com/tuvarna/delivery/delivery/repository/DeliveryRepository.java
@@ -6,14 +6,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+
 
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-
-
-    Page<Delivery> findAllByUserUsername(String username, Pageable pageable);
 
     @Query("SELECT d FROM Delivery d " +
             "WHERE (:username IS NULL OR d.user.username = :username) " +
             "AND (:statusId IS NULL OR d.status.id = :statusId) ")
     Page<Delivery> findAndFilterDeliveries(String username, Long statusId, Pageable pageable);
+
+    @Query("SELECT d FROM Delivery d " +
+            "WHERE d.user.username = :username " +
+            "AND (cast(:afterDate as timestamp) IS NULL OR d.orderedAt >= :afterDate)")
+
+    Page<Delivery> findAndFilterUserDeliveries(String username, LocalDateTime afterDate, Pageable pageable);
 }
