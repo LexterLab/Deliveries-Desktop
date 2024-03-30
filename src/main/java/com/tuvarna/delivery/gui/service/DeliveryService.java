c61e7e20d11e75af4427ee8dc2e96ee2370c6a57
moldovexc <alexanderparpulansky@gmail.com>
2024-03-30 21:51:33 +0200

    fetch request for creating and deleting deliveries

--- src/main/java/com/tuvarna/delivery/gui/service/DeliveryService.java
+++ src/main/java/com/tuvarna/delivery/gui/service/DeliveryService.java
@@ -4,6 +4,7 @@ import com.tuvarna.delivery.delivery.payload.request.DeliveryRequestDTO;
 import com.tuvarna.delivery.delivery.payload.response.DeliveryDTO;
 import com.tuvarna.delivery.delivery.payload.response.DeliveryResponse;
 import com.tuvarna.delivery.gui.utils.AccessTokenStorage;
+import com.tuvarna.delivery.office.payload.request.CourierRequestDTO;
 import com.tuvarna.delivery.utils.AppConstants;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
@@ -50,5 +51,29 @@ public class DeliveryService {
         HttpEntity<String> entity = new HttpEntity<>(headers);
         String url = AppConstants.DOMAIN + "deliveries/" + deliveryId + "/statuses/" + statusId;
         return restTemplate.exchange(url, HttpMethod.PUT, entity, DeliveryDTO.class);
+    }
+
+    public ResponseEntity<Void> fetchDeleteDelivery(Long deliveryId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(AccessTokenStorage.retrieveAccessToken());
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String url = AppConstants.DOMAIN + "deliveries/" + deliveryId ;
+
+        return restTemplate.exchange(url, HttpMethod.DELETE, entity, Void.class);
+    }
+
+    public ResponseEntity<DeliveryRequestDTO> requestDelivery(DeliveryRequestDTO requestDTO) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(AccessTokenStorage.retrieveAccessToken());
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<DeliveryRequestDTO> entity = new HttpEntity<>(requestDTO, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = AppConstants.DOMAIN + "deliveries";
+        return restTemplate.exchange(url, HttpMethod.POST, entity, DeliveryRequestDTO.class);
     }
 }
